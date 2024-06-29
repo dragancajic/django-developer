9dc43c8713297983f290a9a4b8b4c7d3d2f5864d
Драган Ћајић <cajic_dragan@yahoo.com>
2024-06-29 22:55:07 +0200

    ~ `portfolio`: create `Model`s for your application :building_construction: :file_cabinet:
    → A `model` is essentially just a Python class that can be saved into a database.
    › create model `Job` for the `jobs` application
    
    → Pillow just allows Django to work with images much simpler. :framed_picture:
    
    '''install pillow for simpler manipulation with images from Django'''
    (.venv) dragancajic@ubuntu:~/django-developer$ pip install pillow
    ...
    Installing collected packages: pillow
    Successfully installed pillow-10.3.0
    
    '''update your `requirements.txt` file (it now has 18 libraries!)''' √
    (.venv) dragancajic@ubuntu:~/django-developer$ python -m pip freeze > requirements.txt
    
    → `djLint`: HTML template formatter and linter (Django, Jinja, Nunjucks, Twig, Handlebars, Mustache)
    
    '''install `djLint` into your Python virtual environment ([.]venv) | djlint v2024.5.2 (1.34.1)'''
    (.venv) dragancajic@ubuntu:~/django-developer$ python -m pip install --upgrade djlint
    
    '''update your `requirements.txt` file (it now has 30 libraries!)''' √
    (.venv) dragancajic@ubuntu:~/django-developer$ python -m pip freeze > requirements.txt

--- 4_building-portfolio/portfolio-project/jobs/models.py
+++ 4_building-portfolio/portfolio-project/jobs/models.py
@@ -1,3 +1,10 @@
+"""Models for the `jobs` application"""
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    '''Model is just a Python class that can be saved into a database.'''
+    # image property ---> Images
+    image = models.ImageField(upload_to='images/')
+    # summary property -> Summary
+    summary = models.CharField(max_length=200)
