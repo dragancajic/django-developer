6d25f2ba09044ca33b97f22f45da6879b818b41e
Драган Ћајић <cajic_dragan@yahoo.com>
2024-07-01 17:48:25 +0200

    ~ `portfolio`: pull object(s) from database
    › Model-View-Template (MVT)
    
    ∙ MODEL & VIEW
    
    ``` python
    """
        ~ views for your `jobs` application
    """
    from django.shortcuts import render
    from .models import Job
    
    '''Create your views here.'''
    def home(request):
        """
        View for your Home/Index page
        """
        '''jobs = Job.objects'''
        '''or like this'''
        jobs = Job.objects.all()
        return render(request, 'jobs/home.html', {'jobs': jobs})
    ```
    
    › in Django HTML file / template, represent object from DB
    
    ∙ TEMPLATE
    
    <!DOCTYPE html>
    <html lang="en">
    <head>
    	<meta charset="UTF-8">
    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
    	<title>Portfolio Website</title>
    </head>
    <body>
    	<h1>Hello, Драган!</h1>
    	<h2>My jobs and projects:</h2>
    	{% comment %} {% for job in jobs.all %} {% endcomment %}
    	{% comment "or like this" %}{% endcomment %}
    	{% for job in jobs %}
    		<p>{{ job.summary }}</p>
    	{% endfor %}
    </body>
    </html>

--- 4_building-portfolio/portfolio-project/jobs/views.py
+++ 4_building-portfolio/portfolio-project/jobs/views.py
@@ -2,10 +2,14 @@
     ~ views for your `jobs` application
 """
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 def home(request):
     """
     View for your Home/Index page
     """
-    return render(request, 'jobs/home.html')
+    # jobs = Job.objects
+    # or like this
+    jobs = Job.objects.all()
+    return render(request, 'jobs/home.html', {'jobs': jobs})
